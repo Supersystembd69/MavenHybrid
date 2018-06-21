40f0a785da057ed3c3df9734a518ef0365f9f14e
Supersystembd69 <supersystembd69@yahoo.com>
2018-06-20 21:42:37 -0400

    All Files Added

---
+++ HybridMavenProject/src/test/java/operations/AbsatrctClass.java
@@ -0,0 +1,24 @@
+package operations;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+public abstract class AbsatrctClass {
+	
+	protected static WebDriver driver;
+	@BeforeTest
+	public void SetUp() {
+		System.setProperty("webdriver.chrome.driver", "E:\\All Driver\\chromedriver.exe");
+    	driver = new ChromeDriver();
+    	//driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+	}
+	
+
+}
